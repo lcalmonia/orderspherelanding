fd31acf846cbb1ba39617d5da141afee1f817336
lcalmonia <166729703+lcalmonia@users.noreply.github.com>
2026-09-06 09:15:36 +0800

    Route /admin to Ordersphere admin dashboard

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
+import AdminApp from './AdminApp.tsx';
 import './index.css';
+
+const Root = window.location.pathname.replace(/\/+$/, '') === '/admin' ? AdminApp : App;
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Root />
   </StrictMode>,
 );
